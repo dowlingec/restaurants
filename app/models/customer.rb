e5831e7a8adba76a292ce0ed254135e92596bd81
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-05 14:16:19 -0400

    working thru

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,5 +7,17 @@ class Customer < ActiveRecord::Base
     end
 
     def favorite_restaurant
+        puts "iterates over review star ratings, returns name of highest"
+        self.reviews.map do |review|
+            "Rating of this rastaurant is:  #{review.star_rating}"
+        end
+    end
+
+    def add_review
+        puts 'These are not the methods you are looking for'
+    end
+
+    def delete_reviews
+        puts 'These are not the methods you are looking for'
     end
 end
