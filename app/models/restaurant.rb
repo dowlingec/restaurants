6aa45f1da522dbfecee43995cb119cf70c2b9c95
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-05 14:45:14 -0400

    Restaurant class fanciest method added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,11 +2,8 @@ class Restaurant < ActiveRecord::Base
     has_many :reviews
     has_many :customers, through: :reviews
 
-    def fanciest
-        # puts 'These are not the methods you are looking for'
-        #maps over prices of each restaurant
-        #reuturns one whole instance (highest price)
-        self.price.map
+    def self.fanciest
+        Restaurant.all.order(:price).last
     end
 
     def all_reviews
