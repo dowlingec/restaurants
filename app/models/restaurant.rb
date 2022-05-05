41f03ce9e4d8778c588dd9a360dccc6c931c29c9
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-05 13:59:30 -0400

    Restaurant all_reviews method added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,14 @@
 class Restaurant < ActiveRecord::Base
     has_many :reviews
     has_many :customers, through: :reviews
+
+    def fanciest
+        puts 'These are not the methods you are looking for'
+    end
+
+    def all_reviews
+        self.reviews.map do |rev|
+            rev.full_review
+        end
+    end
 end
