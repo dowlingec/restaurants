1d43582d1a2a929cd9a88969121ad6b163b5211c
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-05 13:51:04 -0400

    Customer full_name method created

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ActiveRecord::Base
     has_many :reviews
     has_many :restaurants, through: :reviews
 
-    def
+    def full_name
+        "#{self.first_name} #{self.last_name}"
     end
 end
